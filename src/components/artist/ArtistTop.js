9975944dbea1682870790b528f8d74c05faed2f0
Your Name <you@example.com>
2024-12-17 20:00:38 +0530

    add(17/12/24)

--- src/components/artist/ArtistTop.js
+++ src/components/artist/ArtistTop.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import "../../assets/styles/artist/artisttop.css";
+import artisttop from "../../assets/images/artisttop.jpeg";
 
 const ArtistTop = () => {
   return (
-    <div>
-      hey
+    <div className="artisttop-container">
+    <div className="artisttop_left-side">
+      <h5>FOR ARTISTS</h5>
+      <h1>Music in your <br/>
+      hands. Without the<br/> barriers.</h1>
+      <h4>Full-service production, a global retail <br/>
+      network, customer support - and you keep<br/> up to 85% of profits.</h4>
     </div>
+    <div className="artisttop_right-side">
+      <img src={artisttop} alt="Artist" />
+    </div>
+  </div>
   );
 }
 
